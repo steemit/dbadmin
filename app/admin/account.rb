cabb397354f4e31a101764549d3afbfa13a6de4b
valzav <i@valzav.com>
2016-09-20 18:38:00 -0400

    add support for account.ignored field

--- app/admin/account.rb
+++ app/admin/account.rb
@@ -5,12 +5,23 @@ ActiveAdmin.register Account do
     column :name
     column :remote_ip
     column :referrer
+    column :ignored, as: :check_box
     column :created_at
     actions defaults: false do |a|
       item 'View', admin_account_path(a)
+      item 'Edit', edit_admin_account_path(a)
     end
   end
+
   filter :name
   filter :referrer
   filter :created_at
+
+  form do |f|
+    f.inputs "Account Details" do
+      f.input :ignored
+    end
+    f.actions
+  end
+  permit_params :ignored
 end
