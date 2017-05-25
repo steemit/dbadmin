f7d3ece11a1f5e3c147cfd7cc59bc43488191566
mcifani <mcifani@users.noreply.github.com>
2017-05-25 15:15:05 -0400

    updates

--- app/models/user_attributes.rb
+++ app/models/user_attributes.rb
@@ -1,5 +1,5 @@
 class UserAttributes < ActiveRecord::Base
-  self.table_name = 'user_attributes'
+  # self.table_name = 'user_attributes'
   scope :referer_only, -> {where type_of: 'referer'}
 
   belongs_to :user
