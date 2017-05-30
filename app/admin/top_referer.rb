de41d6e4e470162e95a99d7599a08d86e593fc70
mcifani <mcifani@users.noreply.github.com>
2017-05-30 12:47:35 -0400

    Limit for 100 results

--- app/admin/top_referer.rb
+++ app/admin/top_referer.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register_page "Top Refer" do
 
   content title: "Top 100 Clicked Links/Sources" do
-    table_for UserAttributes.referer_only.group(:value).count.sort.reverse.each do
+    table_for UserAttributes.referer_only.group(:value).count.sort.reverse.first(100).each do
       column "count" do |r|
         r.count
       end
