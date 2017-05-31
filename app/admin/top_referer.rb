ba927421a7627470227552a540ec72185dd139ba
mcifani <mcifani@users.noreply.github.com>
2017-05-31 13:45:13 -0400

    sort correctly on null objects

--- app/admin/top_referer.rb
+++ app/admin/top_referer.rb
@@ -1,13 +1,14 @@
 ActiveAdmin.register_page "Top Refer" do
 
   content title: "Top 100 Clicked Links/Sources" do
-    table_for UserAttributes.referer_only.group(:value).count.sort.reverse.first(100).each do
+    table_for UserAttributes.referer_only.group(:value).count.sort { |x,y| x[0] <=> y[0] }.reverse.first(100).each do
       column "count" do |r|
-        r.count
+        r[1]
       end
       column "value" do |r|
-        r.first
+        r[0]
       end
     end
   end
+
 end
