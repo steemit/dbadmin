04d5465df3db37a823251b9239c9079e5f2fb1f7
mcifani <mcifani@users.noreply.github.com>
2017-06-01 16:53:17 -0400

    Updates

--- app/admin/top_referer.rb
+++ app/admin/top_referer.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register_page "Top Refer" do
 
   content title: "Top 100 Clicked Links/Sources" do
-    table_for UserAttributes.referer_only.value_only.group(:value).count.sort.reverse.first(100).each do
+    table_for UserAttributes.referer_only.value_only.group(:value).count.sort_by{|x| x[1]}.first(100).reverse.each do
       column "count" do |r|
         r[1]
       end
