591e7ad207c5330c7060a0fdd09c1b0406a50abc
mcifani <mcifani@users.noreply.github.com>
2017-06-01 16:31:55 -0400

    Tweak scopeing for attribute tab

--- app/models/user_attributes.rb
+++ app/models/user_attributes.rb
@@ -1,6 +1,7 @@
 class UserAttributes < ActiveRecord::Base
   # self.table_name = 'user_attributes'
   scope :referer_only, -> {where type_of: 'referer'}
+  scope :value_only, -> { where("value <> ''") }
 
   belongs_to :user
 end
