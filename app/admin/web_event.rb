9dcb22004b45686c44facd611b5ac6545af0ebd4
valzav <i@valzav.com>
2017-06-23 18:22:51 -0400

    add web_events tab

---
+++ app/admin/web_event.rb
@@ -0,0 +1,15 @@
+ActiveAdmin.register WebEvent do
+  menu label: 'Events'
+  index do
+    column :id
+    column :event_type
+    column :value
+    column :created_at
+    actions defaults: false do |a|
+      item 'View', admin_web_event_path(a)
+    end
+  end
+
+  filter :event_type, as: :check_boxes, collection: ['csp_violation', 'client_error', 'api/login_account', 'login_attempt']
+
+end
