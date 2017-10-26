cbd4cd656f26071e44e26a1ae66b51fcbf0c6031
valzav <i@valzav.com>
2017-10-26 19:16:07 -0400

    check if account recovery record matches user's email in addition to identity's email

--- app/models/arec.rb
+++ app/models/arec.rb
@@ -58,7 +58,7 @@ class Arec < ActiveRecord::Base
     account = Account.where(name: self.account_name).first
     return false unless account
     eid = account.user.email_identity
-    return (eid and eid.email and eid.email == self.contact_email)
+    return (eid and eid.email and (eid.email == self.contact_email or account.user.email == self.contact_email))
   end
 
   def account
